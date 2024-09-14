cb0e0bc00d251873417b2ee19cde650b5ce3ad89
Xeight <ex8c8r@gmail.com>
2024-09-14 13:54:22 +0500

    commence the cinemavision

--- README.md
+++ README.md
@@ -1,69 +1,27 @@
-# FNF v3
-> Hottest mod ever made -michael yiik
+helloe we hit single
 
-Have you played Friday Night Funkin' and thought "hmm, i wonder when they will update the gaem?" or maybe you played [FNF V2](https://gamebanana.com/wips/84226) and wanted more? We gotchu
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
 
-This mod is everything your heart could possibly desire - New and improved sprites, songs, engine.
+Havee fun (engine still in *beta* btw, ***[follow me on twitter](https://twitter.com/DuskieWhy)***)
 
-![KadeEngineLogo](https://raw.githubusercontent.com/poec987/FNF-V3/main/KadeEngineLogo.png)
-
-# Friday Night Funkin Cinema Engine
-
-# Features
-## Chart Editor (Peak)
-
-*Hate the UGLY and UNUSABLE kade engine chart editor?*
-> WHAT DO YOU MEAN YOU USE PSYCH ENGINE?!
-
-- You can have custom notes with parameters for maximum customization.
-
-- Also, clear chart button (so cool)
-
-- You can like set song to be pixel, good (good option only makes sense for FNF V3 lol, gonna prob be replaced with "ui skins" in standalone) and set whether or not the song has dialogue (that classic pixel dialogue shit frfr) and uhh you can set the stage and gf (not implemented lolllol)*
-
-## Custom Dialogue Shits
-
-All controlled by txt files and pngs and xmls, you can customize your dialogue for some cool dialogue. 
-
-You can make custom character portraits and boxes. What makes shit unique is you can have as many dialogues txts in one song for randomized dialogue!! (woo!)
-
-## Character Editor
-
-Self explanatory. BUTTTT!!! you can make specific animations be flipped on either x, y or both axises!!!! You can also flip the characters on x and y axis as a whole, scale them, set pixel and icons!
-## Stage Editor
-
-Basically character editor, but for stages, idk what to say lmao
-
-# Note
-
-This is a **MOD**. This is not Vanilla and should be treated as a **MODIFICATION**. This will probably never be official, so dont get confused.
-
-## Acknowledging stolen code
-
-[DNB Background Generator](https://github.com/silkycell/DNB-Background-Generator) - The shader code (shaders come from [Vs. Dave and Bambi](https://gamebanana.com/mods/43201))
-
-[Psych Engine](https://github.com/ShadowMario/FNF-PsychEngine) - Alphabet, Setup files, and other shit I prob wouldn't be able to remember.
-
-### Compiling game
-
-To run it from your desktop (Windows, Mac, Linux) it can be a bit more involved. For Linux, you only need to open a terminal in the project directory and run 'lime test linux -debug' and then run the executible file in export/release/linux/bin. For Windows, you need to install Visual Studio Community 2019. While installing VSC, don't click on any of the options to install workloads. Instead, go to the individual components tab and choose the following:
-* MSVC v142 - VS 2019 C++ x64/x86 build tools
-* Windows SDK (10.0.17763.0)
-* C++ Profiling tools
-* C++ CMake tools for windows
-* C++ ATL for v142 build tools (x86 & x64)
-* C++ MFC for v142 build tools (x86 & x64)
-* C++/CLI support for v142 build tools (14.21)
-* C++ Modules for v142 build tools (x64/x86)
-* Clang Compiler for Windows
-* Windows 10 SDK (10.0.17134.0)
-* Windows 10 SDK (10.0.16299.0)
-* MSVC v141 - VS 2017 C++ x64/x86 build tools
-* MSVC v140 - VS 2015 C++ build tools (v14.00)
-
-This will install about 22GB of crap, but once that is done you can open up a command line in the project's directory and run `lime test windows -debug`. Once that command finishes (it takes forever even on a higher end PC), you can run FNF from the .exe file under export\release\windows\bin
-As for Mac, 'lime test mac -debug' should work, if not the internet surely has a guide on how to compile Haxe stuff for Mac.
-
-### Additional guides
-
-- [Command line basics](https://ninjamuffin99.newgrounds.com/news/post/1090480)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
+![](https://github.com/evilfuck/images/blob/main/nightmarevision.png)
--- source/Main.hx
+++ source/Main.hx
@@ -1,33 +1,36 @@
 package;
 
+import sys.thread.Thread;
+import flixel.graphics.FlxGraphic;
 import flixel.FlxG;
 import flixel.FlxGame;
 import flixel.FlxState;
+import flixel.util.FlxColor;
+import openfl.Assets;
 import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
-
-import lime.app.Application;
-
-import openfl.events.UncaughtErrorEvent;
-import haxe.CallStack;
-import haxe.io.Path;
-
-import sys.*;
-import sys.io.*;
-
-using StringTools;
+import openfl.text.TextField;
+import openfl.text.TextFormat;
+import openfl.display.StageScaleMode;
+import meta.states.*;
+import meta.data.*;
+import meta.CompilationStuff;
 
 class Main extends Sprite
 {
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
+	var initialState:Class<FlxState> = Init; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	var framerate:Int = 60; // How many frames per second the game should run at.
-	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
+	var skipSplash:Bool = false; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
+	public static var fpsVar:FPSCounter;
+	public static var compilationInformation:TextField;
+	
+	public static var scaleMode:FunkinRatioScaleMode;
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
@@ -57,9 +60,20 @@ class Main extends Sprite
 			removeEventListener(Event.ADDED_TO_STAGE, init);
 		}
 
-		FlxG.save.bind('funkinv3', 'team-cinemamakers/fnfv3');
-
 		setupGame();
+	}
+
+	public static function setScaleMode(scale:String){
+		switch(scale){
+			default:
+				Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
+			case 'EXACT_FIT':
+				Lib.current.stage.scaleMode = StageScaleMode.EXACT_FIT;
+			case 'NO_BORDER':
+				Lib.current.stage.scaleMode = StageScaleMode.NO_BORDER;
+			case 'SHOW_ALL':
+				Lib.current.stage.scaleMode = StageScaleMode.SHOW_ALL;
+		}
 	}
 
 	private function setupGame():Void
@@ -76,57 +90,163 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-		#if !debug
-		initialState = #if desktop TitleState #else HTMLState #end;
-		#end
+		// #if !debug
+		// #if HIT_SINGLE
+		// initialState = meta.states.HitSingleInit;
+		// #else
+		// initialState = TitleState;		
+		// #end
+		// #end
 
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, framerate, framerate, skipSplash, startFullscreen));
-		
+		ClientPrefs.loadDefaultKeys();
+		addChild(new FNFGame(gameWidth, gameHeight, initialState, #if(flixel < "5.0.0")zoom,#end framerate, framerate, skipSplash, startFullscreen));
+
 		#if !mobile
-		addChild(new FPS(10, 3, 0xFFFFFF));
-		addChild(new RAM(10, 18));
+		fpsVar = new FPSCounter(10, 3, 0xFFFFFF);
+		addChild(fpsVar);
+		Lib.current.stage.align = "tl";
+		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
+		if(fpsVar != null) {
+			fpsVar.visible = ClientPrefs.showFPS;
+		}
+		#end
+		
+		// #if !DEBUG_MODE
+		// 	compilationInformation = new TextField();
+		// 	compilationInformation.height = FlxG.stage.stageHeight/2;
+		// 	compilationInformation.width = FlxG.stage.stageWidth;
+		// 	compilationInformation.defaultTextFormat = new TextFormat('_sans', 48, FlxColor.WHITE, null, null, null, null, null, openfl.text.TextFormatAlign.CENTER);
+		// 	compilationInformation.text = Date.now().toString() + '\n' + Sys.environment()["USERNAME"].trim();
+		// 	compilationInformation.alpha = 0.675;
+		// 	addChild(compilationInformation);
+		// #end
+
+
+		#if html5
+		FlxG.autoPause = false;
+		FlxG.mouse.visible = false;
 		#end
 
-		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
+		FlxG.signals.gameResized.add(onResize);
+		FlxG.signals.preStateSwitch.add(onStateSwitch);
+		FlxG.scaleMode = scaleMode = new FunkinRatioScaleMode();
+
+
+
+
+
+	}
+	private static function onStateSwitch() {
+		scaleMode.resetSize();
 	}
 
-	function onCrash(e:UncaughtErrorEvent):Void
-		{
-			var errMsg:String = "";
-			var path:String;
-			var callStack:Array<StackItem> = CallStack.exceptionStack(true);
-			var dateNow:String = Date.now().toString();
-	
-			dateNow = dateNow.replace(" ", "_");
-			dateNow = dateNow.replace(":", "'");
-	
-			path = "./crash/" + "FNFV3_" + dateNow + ".txt";
-	
-			for (stackItem in callStack)
-			{
-				switch (stackItem)
-				{
-					case FilePos(s, file, line, column):
-						errMsg += file + " (line " + line + ")\n";
-					default:
-						#if desktop
-						Sys.println(stackItem);
-						#end
-				}
-			}
-	
-			errMsg += "\nUncaught Error: " + e.error + "\nShitty Game did a SHitty Crsahs\n\n> Crash Handler written by: sqirra-rng";
-			#if desktop
-			if (!FileSystem.exists("./crash/"))
-				FileSystem.createDirectory("./crash/");
-	
-			File.saveContent(path, errMsg + "\n");
-	
-			Sys.println(errMsg);
-			Sys.println("Crash dump saved in " + Path.normalize(path));
-	
-			Application.current.window.alert(errMsg, "it crashed, lol");
-			Sys.exit(1);
-			#end
+
+	static function onResize(w,h) 
+	{
+		final scale:Float = Math.max(1,Math.min(w / FlxG.width, h / FlxG.height));
+		if (fpsVar != null) {
+			fpsVar.scaleX = fpsVar.scaleY = scale;
 		}
+		// if (compilationInformation!=null) {
+
+		// 	compilationInformation.scaleX = compilationInformation.scaleY = Math.max(1,scale);
+		// 	compilationInformation.height = h;
+		// 	compilationInformation.width = w;
+		// 	compilationInformation.y = h/2;
+		// }
+
+		@:privateAccess if (FlxG.cameras != null) for (i in FlxG.cameras.list) if (i != null && i._filters != null) resetSpriteCache(i.flashSprite);
+		if (FlxG.game != null) resetSpriteCache(FlxG.game);
+		
+	}
+	public static function resetSpriteCache(sprite:Sprite):Void
+	{
+		@:privateAccess 
+		{
+			sprite.__cacheBitmap = null;
+			sprite.__cacheBitmapData = null;
+		}
+	}
+}
+
+class FNFGame extends FlxGame
+{
+	private static function crashGame() {
+		null
+		.draw();
+	}
+
+/**
+* Used to instantiate the guts of the flixel game object once we have a valid reference to the root.
+*/
+	override function create(_):Void {
+		try
+			super.create(_)
+		catch (e)
+			onCrash(e);
+	}
+
+	override function onFocus(_):Void {
+		try
+			super.onFocus(_)
+		catch (e)
+			onCrash(e);
+	}
+
+	override function onFocusLost(_):Void {
+		try
+			super.onFocusLost(_)
+		catch (e)
+			onCrash(e);
+	}
+
+	/**
+	* Handles the `onEnterFrame` call and figures out how many updates and draw calls to do.
+	*/
+	override function onEnterFrame(_):Void {
+		try
+			super.onEnterFrame(_)
+		catch (e)
+			onCrash(e);
+	}
+
+	/**
+	* This function is called by `step()` and updates the actual game state.
+	* May be called multiple times per "frame" or draw call.
+	*/
+	override function update():Void {
+		#if CRASH_TEST
+		if (FlxG.keys.justPressed.F9)
+			crashGame();
+		#end
+		try
+			super.update()
+		catch (e)
+			onCrash(e);
+	}
+
+	/**
+	* Goes through the game state and draws all the game objects and special effects.
+	*/
+	override function draw():Void {
+		try
+			super.draw()
+		catch (e)
+			onCrash(e);
+	}
+
+	private final function onCrash(e:haxe.Exception):Void {
+		var emsg:String = "";
+		for (stackItem in haxe.CallStack.exceptionStack(true)) {
+			switch (stackItem) {
+				case FilePos(s, file, line, column):
+					emsg += file + " (line " + line + ")\n";
+				default:
+					Sys.println(stackItem);
+					trace(stackItem);
+			}
+		}
+
+		FlxG.switchState(new meta.states.substate.CrashReportSubstate(FlxG.state, emsg, e.message));
+	}
 }
