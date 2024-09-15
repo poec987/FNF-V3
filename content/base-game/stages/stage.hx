e06cc680d54b1f2c182e38ce6776fc9b676d09be
Xeight <ex8c8r@gmail.com>
2024-09-15 11:03:04 +0500

    asset stuff

--- content/base-game/stages/stage.hx
+++ content/base-game/stages/stage.hx
@@ -1,14 +1,14 @@
 function onLoad() {
     var bg:FlxSprite = new FlxSprite(-600, -200);
-    bg.loadGraphic(Paths.image("stageback"));
+    bg.loadGraphic(Paths.image("stages/stage/stageback"));
 	add(bg); 
 
     var stageFront:FlxSprite = new FlxSprite(-600, 600);
-    stageFront.loadGraphic(Paths.image("stagefront"));
+    stageFront.loadGraphic(Paths.image("stages/stage/stagefront"));
     add(stageFront);
 
     var stageCurtains:FlxSprite = new FlxSprite(-600, -300);
-    stageCurtains.loadGraphic(Paths.image("stagecurtains"));
+    stageCurtains.loadGraphic(Paths.image("stages/stage/stagecurtains"));
     foreground.add(stageCurtains);
 
     trace("DICK");
