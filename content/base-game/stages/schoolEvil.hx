8f3afff3eb7576d4adbb98793c343c94d4066f32
Xeight <ex8c8r@gmail.com>
2024-12-15 07:39:17 +0500

    revert fuckingg everything and refix stages

--- content/base-game/stages/schoolEvil.hx
+++ content/base-game/stages/schoolEvil.hx
@@ -4,19 +4,19 @@ function onLoad(){
     var posX = 400;
     var posY = 200;
     if(!ClientPrefs.lowQuality) {
-        var bg:BGSprite = new BGSprite('weeb/animatedEvilSchool', posX, posY, 0.8, 0.9, ['background 2'], true);
+        var bg:BGSprite = new BGSprite('stages/weeb/animatedEvilSchool', posX, posY, 0.8, 0.9, ['background 2'], true);
         bg.scale.set(6, 6);
         bg.antialiasing = false;
         add(bg);
 
-        bgGhouls = new BGSprite('weeb/bgGhouls', -100, 200, 0.9, 0.9, ['BG freaks glitch instance'], false);
+        bgGhouls = new BGSprite('stages/weeb/bgGhouls', -100, 200, 0.9, 0.9, ['BG freaks glitch instance'], false);
         bgGhouls.setGraphicSize(Std.int(bgGhouls.width * 6));
         bgGhouls.updateHitbox();
         bgGhouls.visible = false;
         bgGhouls.antialiasing = false;
         add(bgGhouls);
     } else {
-        var bg:BGSprite = new BGSprite('weeb/animatedEvilSchool_low', posX, posY, 0.8, 0.9);
+        var bg:BGSprite = new BGSprite('stages/weeb/animatedEvilSchool_low', posX, posY, 0.8, 0.9);
         bg.scale.set(6, 6);
         bg.antialiasing = false;
         add(bg);
