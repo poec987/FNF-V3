bb5dbcbef888470025f9acab2945adee49d1a00d
poec987 <poedev08@outlook.com>
2025-05-06 21:57:37 -0500

    More Investigate WIP

---
+++ content/base-game/noteskins/ps1.hx
@@ -0,0 +1,17 @@
+// this gets the BF noteskin
+function bfSkin() { return "PS1_assets"; }
+
+// this gets the DAD noteskin
+function dadSkin() { return "PS1_assets"; }
+
+// this gets the notesplash skin and offset
+function noteSplash(offsets){ return "noteSplashes"; }
+
+// does ur noteskin have quants ? 
+function quants() { return false; }
+
+// offset notes, receptors and sustains
+function offset(noteOff, strumOff, susOff){}
+
+// this gets the hud suffix
+function hud() { return "-scop"; }
