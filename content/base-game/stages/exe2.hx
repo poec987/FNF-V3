77e925d6261c0de698807c7c3e198e64d62b40ea
Didgie <bradyboy1120@gmail.com>
2025-07-27 01:45:08 -0700

    the other stuff that didnt go thru for some reason

---
+++ content/base-game/stages/exe2.hx
@@ -0,0 +1,44 @@
+var glow:FlxSprite;
+
+function onLoad() {
+	game.precacheList.set('stages/exetwo/exe2back', 'image');
+	game.precacheList.set('stages/exetwo/exe2glow', 'image');
+	game.precacheList.set('stages/exetwo/exe2front', 'image');
+	game.precacheList.set('characters/EXETWO', 'image');
+	game.precacheList.set('characters/bfNervous', 'image');
+
+    var bg:FlxSprite = new FlxSprite(-400, -700).loadGraphic(Paths.image('stages/exetwo/exe2back'));
+	bg.antialiasing = true;
+	bg.scrollFactor.set(0.9, 0.9);
+	bg.scale.set(0.8,0.8);
+	bg.active = false;
+	bg.updateHitbox();
+	add(bg);
+	
+	glow = new FlxSprite(-400, -700).loadGraphic(Paths.image('stages/exetwo/exe2glow'));
+	glow.antialiasing = true;
+	glow.scale.set(0.8,0.8);
+	glow.alpha = 0;
+	glow.updateHitbox();
+	add(glow);
+
+	var fg:FlxSprite = new FlxSprite(-400, -700).loadGraphic(Paths.image('stages/exetwo/exe2front'));
+	fg.scale.set(0.8,0.8);
+	fg.active = false;
+	fg.antialiasing = true;
+	fg.updateHitbox();
+	add(fg);
+}
+
+function onCreatePost() {
+    playerStrums.autoPlayed = true;
+	game.camHUD.visible = false;
+}
+
+function onBeatHit() {
+	if (curBeat >= 72)
+	{
+		glow.alpha = 1;
+		FlxTween.tween(glow, {alpha: 0}, 0.3);
+	}
+}
